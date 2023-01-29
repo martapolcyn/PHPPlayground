14efa09cb9a16603f77aadca2d9811c8109b3af8
martapolcyn <marta.anna.polcyn@gmail.com>
2023-01-29 19:42:54 +0100

    finish administration panel

--- Library/adminpanel.php
+++ Library/adminpanel.php
@@ -14,6 +14,38 @@ if (isset($_SESSION["user_id"])) {
     $user = $result->fetch_array();
 }
 
+
+// tutaj
+
+if (isset($_POST["add"])) {
+    $title = $_POST["title"];
+    $author = $_POST["author"];
+
+    $query = "INSERT INTO books (title, author)
+              VALUES ('$title', '$author')";
+
+    if ($mysqli->query($query)) {
+        echo "Dodano!";
+    } else {
+        echo "Error: " . $mysqli->error;
+    }
+}
+
+if (isset($_POST["remove"])) {
+    $id = $_POST["id"];
+
+    $query = "DELETE FROM books WHERE id = $id";
+
+    if ($mysqli->query($query)) {
+        echo "Usunięto!";
+    } else {
+        echo "Error: " . $mysqli->error;
+    }
+}
+
+$query = "SELECT * FROM books";
+$result = $mysqli->query($query);
+
 ?>
 
 
@@ -23,6 +55,7 @@ if (isset($_SESSION["user_id"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
     <title>Home</title>
 </head>
 <body>
@@ -30,15 +63,51 @@ if (isset($_SESSION["user_id"])) {
     
     <?php if (isset($user) AND $user['utype']=="admin"): ?>
         
-        <p style="color:green"> Jesteś adminem!</p>
-        <p><a href="logout.php">Wyloguj</a></p>
+        <h2>Lista dostępnych książek</h2>
+        <!-- <p style="color:green"> Jesteś adminem!</p> -->
+        <br><br>
+        <table>
+        <tr>
+            <th>ID</th>
+            <th>Tytuł</th>
+            <th>Autor</th>
+        </tr>
+
+        <?php while ($book = $result->fetch_assoc()) : ?>
+            <tr>
+                <td><?php echo $book["id"]; ?></td>
+                <td><?php echo $book["title"]; ?></td>
+                <td><?php echo $book["author"]; ?></td>
+            </tr>
+        <?php endwhile; ?>
+        </table>
+        <br><br>
+
+        <h3>Dodaj nową pozycję</h3>
+        <form action="" method="post">
+            <input type="text" name="title" placeholder="Tytuł">
+            <input type="text" name="author" placeholder="Autor">
+            <input type="submit" name="add" value="Dodaj">
+        </form>
+
+        <h3>Usuń pozycję</h3>
+        <form action="" method="post">
+            <input type="number" name="id" placeholder="ID">
+            <input type="submit" name="remove" value="Usuń">
+        </form>
+
+
         
     <?php else: ?>
-        
-        <p style="color:red">Nie masz autoryzacji do tej strony</p>
-        <p><a href="login.php">Zaloguj się</a> lub <a href="signup.html">Załóż konto</a></p>
+        <br>
+        <h2 style="color:red">Nie masz autoryzacji do tej strony</h2>
+        <br><br>
+        <h4><a href="login.php">Zaloguj się jako administrator</a></h4>
         
     <?php endif; ?>
+
+    <br>
+    <h4><a href="logout.php">Wyloguj</a></h4>
 </body>
 </html>
 
--- Library/index.php
+++ Library/index.php
@@ -23,22 +23,37 @@ if (isset($_SESSION["user_id"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
     <title>Home</title>
 </head>
 <body>
 <h1>Biblioteka</h1>
     
     <?php if (isset($user)): ?>
-        
-        <p>Witaj <?= htmlspecialchars($user["firstname"]) ?></p>
+        <br>
+        <h3>Witaj <?= htmlspecialchars($user["firstname"]) ?></h3>
         <p>Twoje dane: </p>
-
-        <p>Numer karty: <?= htmlspecialchars($user["id"]) ?></p>
-        <p>Imię: <?= htmlspecialchars($user["firstname"]) ?></p>
-        <p>Nazwisko: <?= htmlspecialchars($user["lastname"]) ?></p>
-        <p>Email: <?= htmlspecialchars($user["email"]) ?></p>
-        
-        <p><a href="logout.php">Wyloguj</a></p>
+        <br>
+        <table>
+        <tr>
+            <th>Numer karty</th>
+            <td><?= htmlspecialchars($user["id"]) ?></td>
+        </tr>
+        <tr>
+            <th>Imię</th>
+            <td><?= htmlspecialchars($user["firstname"]) ?></td>
+        </tr>
+        <tr>
+            <th>Nazwisko</th>
+            <td><?= htmlspecialchars($user["lastname"]) ?></td>
+        </tr>
+        <tr>
+            <th>Email</th>
+            <td><?= htmlspecialchars($user["email"]) ?></td>
+        </tr>
+        </table>
+        <br>
+        <h4><a href="logout.php">Wyloguj</a></h4>
         
     <?php else: ?>
         
